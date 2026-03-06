39b1f0bcc335b90a6145a8263aa1680d9483f796
frankcholula <lu.phrank@gmail.com>
2026-03-06 13:29:43 +0000

    kicked off pretrain vae frozen + DiT

--- scripts/pretrain/dit_vae_dryrun.sh
+++ scripts/pretrain/dit_vae_dryrun.sh
@@ -9,9 +9,12 @@ python gaussianwm/train_diffusion.py \
     --config-name train_dit_with_vae \
     dataset=$DATASET \
     dataset.data_path=$DATA_PATH \
-    output_dir=$GWM_PATH/logs/gwm_vae_dryrun \
-    log_dir=$GWM_PATH/logs/gwm_vae_dryrun \
+    world_model.vae.checkpoint_path=$GWM_PATH/logs/vae_m512/checkpoint-19.pth \
+    world_model.vae.freeze=true \
+    train.update_tokenizer=false \
     train.max_steps=10 \
     train.save_every=5 \
     train.log_every=1 \
+    output_dir=$GWM_PATH/logs/gwm_vae_dryrun \
+    log_dir=$GWM_PATH/logs/gwm_vae_dryrun \
     use_wandb=false
--- scripts/pretrain/dit_vae_single_gpu.sh
+++ scripts/pretrain/dit_vae_single_gpu.sh
@@ -9,5 +9,9 @@ python gaussianwm/train_diffusion.py \
     --config-name train_dit_with_vae \
     dataset=$DATASET \
     dataset.data_path=$DATA_PATH \
+    world_model.vae.checkpoint_path=$GWM_PATH/logs/vae_m512/checkpoint-19.pth \
+    world_model.vae.freeze=true \
+    train.update_tokenizer=false \
     output_dir=$GWM_PATH/logs/gwm_vae \
-    log_dir=$GWM_PATH/logs/gwm_vae
+    log_dir=$GWM_PATH/logs/gwm_vae \
+    wandb.name=dit_frozen_vae
